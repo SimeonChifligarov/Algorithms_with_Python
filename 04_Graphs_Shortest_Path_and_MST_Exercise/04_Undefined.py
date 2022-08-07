9114b1a09e0cd470904215ecfd16a4b734bb0c1b
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-08-07 19:06:54 +0300

    adding 04_Undefined

---
+++ 04_Graphs_Shortest_Path_and_MST_Exercise/04_Undefined.py
@@ -0,0 +1,49 @@
+# Bellman-Ford Algorithm
+from collections import deque
+
+
+def find_path(parent, node):
+    result = deque()
+    while node is not None:
+        result.appendleft(node)
+        node = parent[node]
+    return result
+
+
+nodes = int(input())
+edges = int(input())
+
+graph = []
+
+for _ in range(edges):
+    first, second, weight = [int(el) for el in input().split()]
+    graph.append((first, second, weight))
+
+source = int(input())
+destination = int(input())
+
+distance = [float('inf')] * (nodes + 1)
+distance[source] = 0
+
+parent = [None] * (nodes + 1)
+
+for _ in range(nodes - 1):
+    for first, second, weight in graph:
+        if distance[first] == float('inf'):
+            continue
+        new_distance = distance[first] + weight
+        if new_distance < distance[second]:
+            distance[second] = new_distance
+            parent[second] = first
+
+for first, second, weight in graph:
+    if distance[first] == float('inf'):
+        continue
+    new_distance = distance[first] + weight
+    if new_distance < distance[second]:
+        print('Undefined')
+        break
+else:
+    path = find_path(parent, destination)
+    print(*path)
+    print(distance[destination])
